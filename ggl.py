b9a1c211e1a643fdd93b8ba1b5ac2b470149e075
olehandro <odpash.itmo@gmail.com>
2023-07-16 16:22:04 +0300

    Initial commit

---
+++ ggl.py
@@ -0,0 +1,69 @@
+import dataclasses
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+@dataclasses.dataclass
+class SearchGroup:
+    name: str
+    last_update: str
+    chat_id: str
+    link: str
+
+
+@dataclasses.dataclass
+class SearchResult:
+    chat_id: str
+    from_group: str
+    name: str
+    surname: str
+    username: str
+    phone: str
+    message_time: str
+    message_text: str
+
+
+
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('google.json', scope)
+search_group_page_id = 541067323
+keywords_page_id = 391680335
+search_result_page_id = 221888722
+
+def get_chats() -> list[SearchGroup]:
+    client = gspread.authorize(creds)
+    sheet = client.open('ЧАТ').get_worksheet_by_id(search_group_page_id)
+    values = sheet.get_all_values()[1::]
+    resp = []
+    for v in values:
+        resp.append(SearchGroup(name=v[1], last_update=v[2], chat_id=v[3], link=v[0]))
+    return resp
+
+
+def get_keywords() -> list[str]:
+    client = gspread.authorize(creds)
+    sheet = client.open('ЧАТ').get_worksheet_by_id(keywords_page_id)
+    values = sheet.get_all_values()
+    resp = []
+    for v in values:
+        resp.append(v[0])
+    return resp
+
+
+def write_result(result: SearchResult):
+    print(result)
+    client = gspread.authorize(creds)
+    sheet = client.open('ЧАТ').get_worksheet_by_id(search_result_page_id)
+    sheet.append_row([result.chat_id, result.from_group, result.name, result.surname,
+                     result.username, result.phone, result.message_time, result.message_text])
+
+
+def update_group_info(group: SearchGroup):
+    client = gspread.authorize(creds)
+    sheet = client.open('ЧАТ').get_worksheet_by_id(search_group_page_id)
+    values = sheet.get_all_values()
+    for row in range(len(values)):
+        if values[row][0] == group.link:
+            sheet.update_cell(row + 1, 2, group.last_update)
+            sheet.update_cell(row + 1, 3, group.chat_id)
+            sheet.update_cell(row + 1, 4, group.name)
+            break
---
+++ main.py
@@ -0,0 +1,77 @@
+import time
+
+from telethon import TelegramClient
+from telethon.tl.functions.messages import ImportChatInviteRequest
+
+import ggl
+from ggl import SearchResult
+import asyncio
+from ggl import SearchGroup
+
+
+async def main():
+    api_id = 2724818
+    api_hash = '6c677b0f0e2af14a53cbf0c0eafe5886'
+    client = TelegramClient('clients_scraper', api_id, api_hash)
+    await client.start()
+    await client.connect()
+    print('Connected')
+    while True:
+        dialogs = await client.get_dialogs()
+        print('Getted Dialogs')
+        chats: list[SearchGroup] = ggl.get_chats()
+        keywords: list[str] = ggl.get_keywords()
+        print('Readed chats')
+        for chat in chats:
+            # Adding new position
+
+            if chat.chat_id == '':
+                print('Adding new Chat')
+                data = await client(ImportChatInviteRequest(chat.link.split('+')[1]))
+                chat.name = data.chats[0].title
+                chat.chat_id = data.chats[0].id
+                dialogs = await client.get_dialogs()
+
+            # Finding dialog
+            print('Finding dialog')
+            for dialog in dialogs:
+                if dialog.name == chat.name:
+                    break
+            print('Reading messages')
+            async for message in client.iter_messages(dialog):  # Reading messages
+                try:
+                    message_time = message.date.timestamp()
+                    if chat.last_update == '':
+                        chat.last_update = 0
+                    if int(message_time) < chat.last_update:
+                        break
+                    message_text = message.message
+                    keyword_contains = False
+                    # Trying to find keywords
+                    for keyword in keywords:
+                        if keyword.lower() in message_text.lower():
+                            keyword_contains = True
+
+                    if keyword_contains:
+                        print('Keyword found!')
+                        user_id = message.from_id.user_id
+                        # Getting participant
+                        print('Getting participant')
+                        participant = await client.get_entity(int(user_id))
+                        print(participant)
+                        # Writing result
+                        print('Writing result')
+                        ggl.write_result(SearchResult(chat_id=participant.id, from_group=chat.name, message_text=message_text,
+                                                      message_time=str(message.date), name=participant.first_name,
+                                                      surname=participant.last_name, phone=participant.phone,
+                                                      username=participant.username))
+
+                except Exception as e:
+                    print(e)
+            chat.last_update = int(time.time())
+            ggl.update_group_info(chat)  # Updating group information
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
